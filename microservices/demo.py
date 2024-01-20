95357c8b7756959eda137eff0baf073febb84d1e
gauravwarise <gauravwarise04@gmail.com>
2024-01-20 22:47:32 +0530

    fixed microservice send messages in loop

--- microservices/demo.py
+++ microservices/demo.py
@@ -1,18 +1,26 @@
 import asyncio
 import websockets
 import json
+import time
 
 async def connect_to_websocket():
     uri = "ws://localhost:8000/ws/demo/"  # Replace with your WebSocket URL
 
     async with websockets.connect(uri) as websocket:
+        while True:
         # Sending a message
-        message_to_send = json.dumps({"message":"Hello, WebSocket!"})
-        await websocket.send(message_to_send)
-        print(f"Sent: {message_to_send}")
+            counter = 0
+            message_to_send = json.dumps({"message":f"Hello, WebSocket!{counter}"})
+            counter = counter+1
+            await websocket.send(message_to_send)
+            print(f"Sent: {message_to_send}")
+            try:
+                # Receiving a message
+                message_received = await websocket.recv()
+                print(f"Received: {message_received}")
+            except asyncio.TimeoutError:
+                print("No message received in the last 5 seconds.")
+            await asyncio.sleep(5)
 
-        # Receiving a message
-        message_received = await websocket.recv()
-        print(f"Received: {message_received}")
 
 asyncio.get_event_loop().run_until_complete(connect_to_websocket())
